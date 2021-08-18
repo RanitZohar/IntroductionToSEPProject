260a5ca6f1f99dac72df0050334f097c6f69a076
RanitZohar <73180703+RanitZohar@users.noreply.github.com>
2021-08-18 19:02:57 +0300

    Add files via upload

---
+++ PL/PO/Enums.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PO
+{
+    public enum STATUS
+    {
+        READYFORRIDE, MIDDLERIDE, INTIDLUK, INTREATMENT
+    }
+    public enum AREAS
+    {
+        JERUSALEM, NORTH, SOUTH, GENERAL, CENTER
+    }
+
+}
